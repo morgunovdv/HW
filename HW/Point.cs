d97d5f0579b807286858e73ac5df16f923566adb
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-04-06 14:00:59 +0700

    Добавьте файлы проекта.

---
+++ HW/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW
+{
+    class Point
+    {
+        private int _x;
+        private int _y;
+        private string _name;
+
+        public int X => _x;
+        public int Y => _y;
+        public string Name => _name;
+
+        
+        public Point(int x, int y, string name)
+        {
+            _x = x;
+            _y = y;
+            _name = name;
+        }
+    }
+}
---
+++ HW/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point x = new Point(30, 40, "X");
+            Point y = new Point(60, 80, "Y");
+            Point z = new Point(90, 110, "Z");
+
+            Point a = new Point(int.Parse(Console.ReadLine()), 4, "A");
+
+
+            Polygon triangle = new Polygon(x, y, z);
+
+            string name = triangle.GetName();
+
+            Console.WriteLine($"Ваша фигура: {name}");
+        }
+    }
+}
